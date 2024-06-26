5e415d25752c5820337e0de6cff9f8e4891bd2d0
EzequielRodas <105452530+EzequielRodas@users.noreply.github.com>
2024-06-26 10:00:00 -0300

    Corrección de import LoginPage from '../pages/LoginPage';

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,5 +1,5 @@
 import { test, expect } from '@playwright/test';
-import LoginPage from '../pages/loginPage';
+import LoginPage from '../pages/LoginPage';
 
 test.describe('Regresión Urbetrack', ()=>{
   
